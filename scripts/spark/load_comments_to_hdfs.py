882c66bd26f01136f0c9cf87b9011fe3450391f2
Dzenis Madzovic <dzenis.madzovic@pinmeto.com>
2023-11-22 21:17:10 +0100

    Formatter

--- scripts/spark/load_comments_to_hdfs.py
+++ scripts/spark/load_comments_to_hdfs.py
@@ -8,75 +8,77 @@ import os
 def setup_logging():
     log_directory = "/scripts/logs"
     logging.basicConfig(
-        filename=f'{log_directory}/data_transfer.log',
+        filename=f"{log_directory}/data_transfer.log",
         level=logging.INFO,
-        format='%(asctime)s:%(levelname)s:%(message)s'
+        format="%(asctime)s:%(levelname)s:%(message)s",
     )
 
 
 def transfer_data_to_namenode():
-    hdfs_path = 'hdfs://namenode:9000/user/spark/reddit_comments/'
+    hdfs_path = "hdfs://namenode:9000/user/spark/reddit_comments/"
     spark = None
     try:
-        spinner = Halo(text=f'Starting Spark session for data transfer')
+        spinner = Halo(text=f"Starting Spark session for data transfer")
         logging.info("Starting Spark session for data transfer")
-        spark = SparkSession.builder \
-            .appName('Data transfer to HDFS') \
-            .getOrCreate()
+        spark = SparkSession.builder.appName("Data transfer to HDFS").getOrCreate()
         spinner.succeed("Spark session started successfully")
 
-        schema = StructType([
-            StructField("created_utc", IntegerType(), True),
-            StructField("ups", IntegerType(), True),
-            StructField("subreddit_id", StringType(), True),
-            StructField("link_id", StringType(), True),
-            StructField("name", StringType(), True),
-            StructField("score_hidden", StringType(), True),
-            StructField("author_flair_css_class", StringType(), True),
-            StructField("author_flair_text", StringType(), True),
-            StructField("subreddit", StringType(), True),
-            StructField("id", StringType(), True),
-            StructField("removal_reason", StringType(), True),
-            StructField("gilded", IntegerType(), True),
-            StructField("downs", IntegerType(), True),
-            StructField("archived", StringType(), True),
-            StructField("author", StringType(), True),
-            StructField("score", IntegerType(), True),
-            StructField("retrieved_on", IntegerType(), True),
-            StructField("body", StringType(), True),
-            StructField("distinguished", StringType(), True),
-            StructField("edited", StringType(), True),
-            StructField("controversiality", IntegerType(), True),
-            StructField("parent_id", StringType(), True)
-        ])
+        schema = StructType(
+            [
+                StructField("created_utc", IntegerType(), True),
+                StructField("ups", IntegerType(), True),
+                StructField("subreddit_id", StringType(), True),
+                StructField("link_id", StringType(), True),
+                StructField("name", StringType(), True),
+                StructField("score_hidden", StringType(), True),
+                StructField("author_flair_css_class", StringType(), True),
+                StructField("author_flair_text", StringType(), True),
+                StructField("subreddit", StringType(), True),
+                StructField("id", StringType(), True),
+                StructField("removal_reason", StringType(), True),
+                StructField("gilded", IntegerType(), True),
+                StructField("downs", IntegerType(), True),
+                StructField("archived", StringType(), True),
+                StructField("author", StringType(), True),
+                StructField("score", IntegerType(), True),
+                StructField("retrieved_on", IntegerType(), True),
+                StructField("body", StringType(), True),
+                StructField("distinguished", StringType(), True),
+                StructField("edited", StringType(), True),
+                StructField("controversiality", IntegerType(), True),
+                StructField("parent_id", StringType(), True),
+            ]
+        )
 
         logging.info(f"Reading and writing data from /data/comments.csv to {hdfs_path}")
-        spinner = Halo(text=f"Reading and writing data from /data/comments.csv to {hdfs_path}")
-        df = spark.read \
-            .option('header', 'true') \
-            .option('mode', 'DROPMALFORMED') \
-            .schema(schema) \
-            .csv('/data/comments.csv')
+        spinner = Halo(
+            text=f"Reading and writing data from /data/comments.csv to {hdfs_path}"
+        )
+        df = (
+            spark.read.option("header", "true")
+            .option("mode", "DROPMALFORMED")
+            .schema(schema)
+            .csv("/data/comments.csv")
+        )
 
         logging.info(f"Writing data to {hdfs_path}")
-        df.write \
-            .format('parquet') \
-            .mode('overwrite') \
-            .save(f'{hdfs_path}')
+        df.write.format("parquet").mode("overwrite").save(f"{hdfs_path}")
         logging.info("Data transfer completed successfully")
         spinner.succeed("Data transfer completed successfully")
 
     except IOError as ioe:
-        logging.error(f'IO Error: {ioe}', exc_info=True)
+        logging.error(f"IO Error: {ioe}", exc_info=True)
     except Exception as e:
-        logging.error(f'Unexpected Error: {e}', exc_info=True)
+        logging.error(f"Unexpected Error: {e}", exc_info=True)
     finally:
-        spinner.fail("Data transfer failed. See logs in /scripts/logs/data_transfer.log for details")
+        spinner.fail(
+            "Data transfer failed. See logs in /scripts/logs/data_transfer.log for details"
+        )
         if spark:
             spark.stop()
             logging.info("Spark session stopped")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup_logging()
     transfer_data_to_namenode()
--- scripts/spark/utils/html_utils.py
+++ scripts/spark/utils/html_utils.py
@@ -1,6 +1,6 @@
-def dataframe_to_html_with_css(df, css_path='/css/default-styles.css'):
+def dataframe_to_html_with_css(df, css_path="/css/default-styles.css"):
     html_data = df.to_html()
-    
+
     html_data_with_css = f"""
     <!DOCTYPE html>
     <html>
